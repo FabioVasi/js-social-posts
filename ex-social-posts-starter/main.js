775c75e5cbf00f96bd3068b9437052cf5fd27bd0
FabioVasi <vasifabio96@gmail.com>
2023-09-08 16:10:39 +0200

    add mark up

--- ex-social-posts-starter/main.js
+++ ex-social-posts-starter/main.js
@@ -39,58 +39,120 @@ Se finite la parte logica ed i vari bonus e vi avanza tempo per giocare un pó, 
 
 const posts = [
     {
-        "id": 1,
-        "content": "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
-        "media": "https://unsplash.it/600/300?image=171",
-        "author": {
-            "name": "Phil Mangione",
-            "image": "https://unsplash.it/300/300?image=15"
+        id: 1,
+        content: "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
+        media: "https://unsplash.it/600/300?image=171",
+        author: {
+            name: "Phil Mangione",
+            image: "https://unsplash.it/300/300?image=15"
         },
-        "likes": 80,
-        "created": "2021-06-25"
+        likes: 80,
+        created: "2021-06-25"
     },
     {
-        "id": 2,
-        "content": "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
-        "media": "https://unsplash.it/600/400?image=112",
-        "author": {
-            "name": "Sofia Perlari",
-            "image": "https://unsplash.it/300/300?image=10"
+        id: 2,
+        content: "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
+        media: "https://unsplash.it/600/400?image=112",
+        author: {
+            name: "Sofia Perlari",
+            image: "https://unsplash.it/300/300?image=10"
         },
-        "likes": 120,
-        "created": "2021-09-03"
+        likes: 120,
+        created: "2021-09-03"
     },
     {
-        "id": 3,
-        "content": "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
-        "media": "https://unsplash.it/600/400?image=234",
-        "author": {
-            "name": "Chiara Passaro",
-            "image": "https://unsplash.it/300/300?image=20"
+        id: 3,
+        content: "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
+        media: "https://unsplash.it/600/400?image=234",
+        author: {
+            name: "Chiara Passaro",
+            image: "https://unsplash.it/300/300?image=20"
         },
-        "likes": 78,
-        "created": "2021-05-15"
+        likes: 78,
+        created: "2021-05-15"
     },
     {
-        "id": 4,
-        "content": "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
-        "media": "https://unsplash.it/600/400?image=24",
-        "author": {
-            "name": "Luca Formicola",
-            "image": null
+        id: 4,
+        content: "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
+        media: "https://unsplash.it/600/400?image=24",
+        author: {
+            name: "Luca Formicola",
+            image: null
         },
-        "likes": 56,
-        "created": "2021-04-03"
+        likes: 56,
+        created: "2021-04-03"
     },
     {
-        "id": 5,
-        "content": "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
-        "media": "https://unsplash.it/600/400?image=534",
-        "author": {
-            "name": "Alessandro Sainato",
-            "image": "https://unsplash.it/300/300?image=29"
+        id: 5,
+        content: "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
+        media: "https://unsplash.it/600/400?image=534",
+        author: {
+            name: "Alessandro Sainato",
+            image: "https://unsplash.it/300/300?image=29"
         },
-        "likes": 95,
-        "created": "2021-03-05"
+        likes: 95,
+        created: "2021-03-05"
     }
 ];
+
+console.log(posts);
+
+const rowDomElement = document.querySelector('.posts-list');
+
+for (let i = 0; i < posts.length; i++) {
+
+    const post = posts[i];
+
+    console.log(post);
+
+    console.log(post['id']);
+
+    console.log(post['content']);
+
+    console.log(post['media']);
+
+    console.log(post['author[name, image]']);
+
+    console.log(post['likes']);
+
+    console.log(post['created']);
+
+    // creo una costante per l'aggiunta del markup che voglio visualizzare in pagina
+    const markup = `
+    <div class="post">
+        <div class="post__header">
+            <div class="post-meta">                    
+                <div class="post-meta__icon">
+                    <img class="profile-pic" src="${post['author']}" alt="Phil Mangione">                    
+                </div>
+                <div class="post-meta__data">
+                    <div class="post-meta__author">${post['author']}</div>
+                    <div class="post-meta__time">${post['created']}</div>
+                </div>                    
+            </div>
+        </div>
+        <div class="post__text">${post['content']}</div>
+        <div class="post__image">
+            <img src="${post['media']}" alt="">
+        </div>
+        <div class="post__footer">
+            <div class="likes js-likes">
+                <div class="likes__cta">
+                    <a class="like-button  js-like-button" href="#" data-postid="${post['id']}">
+                        <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+                        <span class="like-button__label">Mi Piace</span>
+                    </a>
+                </div>
+                <div class="likes__counter">
+                    Piace a <b id="like-counter-1" class="js-likes-counter">${post['likes']}</b> persone
+                </div>
+            </div> 
+        </div>            
+    </div>`
+    
+    // loggo il markup in console
+    //console.log(markup);
+    // stampo in pagina le card partendo dalla prima in ordine cronologico
+    rowDomElement.insertAdjacentHTML('beforeend', markup);
+
+}
